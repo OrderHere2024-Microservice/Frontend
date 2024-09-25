7904651a7193935c35f4bf1537a58b515b791ad3
SteveGuoh <hghandle@outlook.com>
2024-09-25 21:22:16 +1000

    update auth & account popover

---
+++ pages/api/auth/next-auth.d.ts
@@ -0,0 +1,19 @@
+import { User } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session {
+    token?: JWT;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    user?: User;
+    account?: {
+      provider: string;
+      providerAccountId: string;
+    };
+    accessToken?: string;
+  }
+}
