321f910e9d2316f775eaf7bd5a1542dc12c5ec8f
SteveGuoh <hghandle@outlook.com>
2024-10-01 23:42:48 +1000

    test

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -57,8 +57,8 @@ export const authOptions: NextAuthOptions = {
       },
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID || '',
-      clientSecret: process.env.GOOGLE_SECRET || '',
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID || '',
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET || '',
       authorization: {
         params: {
           prompt: 'consent',
@@ -67,8 +67,8 @@ export const authOptions: NextAuthOptions = {
       },
     }),
     FacebookProvider({
-      clientId: process.env.FACEBOOK_ID || '',
-      clientSecret: process.env.FACEBOOK_SECRET || '',
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_ID || '',
+      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_SECRET || '',
     }),
   ],
 
